74ef34bb44d28b09bf057ab66c20682e6933f1ee
jane4ka <96070111+jane4ka@users.noreply.github.com>
2024-06-17 23:16:46 +0300

    Update metallica-config.js

--- configs/metallica-config.js
+++ configs/metallica-config.js
@@ -1,7 +1,7 @@
 const reqOptions = {
-    hostname: 'msk.kassir.ru',
+    hostname: 'https://stradarace.ru/',
     port: 443,
-    path: '/koncert/ok-lujniki/metallica-worldwired-tour-2019_2019-07-21',
+    path: '/%D0%B2%D0%B5%D0%BB%D0%BE%D0%B3%D0%BE%D0%BD%D0%BA%D0%B0',
     method: 'GET'
 };
 const domSelector = '.table-price';
